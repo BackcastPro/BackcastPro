4109d4e3b21f42d46834eb3abba6b0965962c3a7
botterYosuke <yosuke.sasazawa@gmail.com>
2025-09-28 13:04:00 +0900

    サンプルデータを合わせた

--- docs/examples/QuickStartUserGuide.py
+++ docs/examples/QuickStartUserGuide.py
@@ -1,7 +1,8 @@
-import os
+from os.path import dirname, join
 import sys
 sys.path.append('../../src')
 
+import pandas as pd
 import pandas_datareader.data as web
 
 from BackcastPro import Strategy
@@ -29,27 +30,28 @@ class SmaCross(Strategy):
             # If sma1 crosses above sma2, close any existing
             # short trades, and buy the asset
             if crossover(df.SMA1, df.SMA2):
+                self.position.close()
                 self.buy(code=code)
 
             # Else, if sma1 crosses below sma2, close any existing
             # long trades, and sell the asset
             elif crossover(df.SMA2, df.SMA1):
+                self.position.close()
                 self.sell(code=code)
 
 
 # データ取得とバックテスト実行
 from BackcastPro import Backtest
 
-# データ取得（例: トヨタ 7203）
-code='7203.JP'
-df = web.DataReader(code, 'stooq')
+# データ取得
+code='GOOG'
+df = pd.read_csv(join(dirname(__file__), f'{code}.csv'),
+                       index_col=0, parse_dates=True)
+
 bt = Backtest({code: df}, SmaCross, cash=10_000, commission=.002, finalize_trades=True)
 stats = bt.run()
 print(stats)
 
-# Streamlit で表示
-page_title = os.path.splitext(os.path.basename(__file__))[0]
-plot(page_title, bt)
 
 
 
